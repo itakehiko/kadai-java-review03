83dd85e204c1b4008c59ab2d0948b4e1202b1197
itakehiko <takehiko.iwasaki.24@gmail.com>
2023-11-22 21:42:33 +0900

    課題　プロ野球チームの勝ち負け数を管理しよう

--- src/baseball/BaseBallTeam.java
+++ src/baseball/BaseBallTeam.java
@@ -2,4 +2,60 @@ package baseball;
 
 public class BaseBallTeam {
 
+    private String name;
+    private int win;
+    private int lose;
+    private int draw;
+    
+    public BaseBallTeam() {
+        
+    }
+    
+    public BaseBallTeam(String name, int win, int lose, int draw) {
+        this.name = name;
+        this.win = win;
+        this.lose = lose;
+        this.draw = draw;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public int getWin() {
+        return win;
+    }
+    
+    public void setWin(int win) {
+        this.win = win;
+    }
+    
+    public int getLose() {
+        return lose;
+    }
+    
+    public void setLose(int lose) {
+        this.lose = lose;
+    }
+    
+    public int getDraw() {
+        return draw;
+    }
+    
+    public void setDraw(int draw) {
+        this.draw = draw;
+    }
+    
+    public double getRate() {
+        return (double) win / (win + lose);
+    }
+    
+    public void report() {
+        System.out.printf("%s の2022年の成績は %s勝 %s敗 %s分、勝率は %sです。\n", name, win, lose, draw, getRate());
+    }
+    
 }
